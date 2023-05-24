6d5bbe435e944bdb1111c2ea2ade67f19661dcdd
Blas Lopez Gimenez <745095@alu.murciaeduca.es>
2023-05-24 23:40:41 +0200

    añado los archivos java

---
+++ LopezGimenez.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.lopezgimenez;
+
+/**
+ *
+ * @author BLAS
+ */
+public class LopezGimenez {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
