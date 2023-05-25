68ca1882a939a5cffcdacc57bd7efaedd9a83f88
Blas Lopez Gimenez <745095@alu.murciaeduca.es>
2023-05-25 19:55:08 +0200

    Nuevos metodos master

--- Gimenez.java
+++ Gimenez.java
@@ -9,4 +9,17 @@ public class Gimenez {
  public void nombreGimenez(){
      System.out.println("Clase gimenez");
     }
+ 
+  public static int aleatorioEntreDosNumeros(int numeroA1, int numeroA2) {
+
+        int aleatorio = (int) (Math.random() * (numeroA1 - numeroA2) + numeroA2);
+        return aleatorio;
+    }
+  
+   public static double precioConIva( double num){
+        double Iva = num * 0.21;
+        double precioTotal = num + Iva;
+
+        return precioTotal;
+    }
 }
--- Lopez.java
+++ Lopez.java
@@ -10,4 +10,17 @@ public class Lopez {
     public void nombreLopez(){
      System.out.println("Clase lopez");
     }
+    
+      public static boolean esMultiplo(int numero, int multiplo) {
+        if (multiplo % numero == 0) {
+            return true;
+        }
+        return false;
+    }
+      
+       public static void numeroAleatorio() {
+        int aleatorio = (int) (Math.random() * 100 + 1);
+        System.out.println("El numero aleatorio es " + aleatorio);
+        
+    }
 }
