ee8dacee7714221e2aa67394c8bb8c8e53933313
Blas Lopez Gimenez <745095@alu.murciaeduca.es>
2023-05-25 19:51:18 +0200

    añado metodos

--- Gimenez.java
+++ Gimenez.java
@@ -1,6 +1,8 @@
 
 package com.mycompany.lopezgimenez;
 
+import java.util.Scanner;
+
 /**
  *
  * @author BLAS
@@ -9,4 +11,24 @@ public class Gimenez {
  public void nombreGimenez(){
      System.out.println("Clase gimenez");
     }
+  public static int leeNumero() {
+        Scanner lee = new Scanner(System.in);
+        int numeroLeido = lee.nextInt();
+        return numeroLeido;
+    }
+  
+   public static int mayorDeTresNumeros(int primerN, int segundoN, int tercerN) {
+
+        int numeroMayor;
+        if ((primerN > segundoN) && (primerN > tercerN)) {
+            numeroMayor = primerN;
+        } else if ((segundoN > primerN) && (segundoN > tercerN)) {
+            numeroMayor = segundoN;
+
+        } else {
+            numeroMayor = tercerN;
+        }
+        return numeroMayor;
+    }
+
 }
--- Lopez.java
+++ Lopez.java
@@ -1,6 +1,8 @@
 
 package com.mycompany.lopezgimenez;
 
+import java.util.Scanner;
+
 /**
  *
  * @author BLAS
@@ -9,5 +11,16 @@ public class Lopez {
     
     public void nombreLopez(){
      System.out.println("Clase lopez");
+    }
+      public static String leeLetra(){
+          Scanner lee = new Scanner(System.in);
+          String letraLeida= lee.next();
+          return letraLeida;
+    }
+      
+       public static double leeNumeroConDecimales() {
+        Scanner lee = new Scanner(System.in);
+        double numeroLeido = lee.nextDouble();
+        return numeroLeido;
     }
 }
